c811df6e1552568178542ae31c5e97c7621b4675
Cameron Alexander <emptyflash@gmail.com>
2016-02-22 15:10:28 -0600

    Fix running circle ci code

--- bot.py
+++ bot.py
@@ -12,21 +12,19 @@ def issue_comment_created(issue_comment):
     comment_body = issue_comment["comment"]["body"]
     if comment_body.lower() == "lgtm":
         pull_request_url = issue_comment["issue"]["pull_request"]["url"]
-	pull_request_number = pull_request_user.split("/")[-1]
+	pull_request_number = pull_request_url.split("/")[-1]
     	pull_request = github.pull_requests.get(
 	    number=pull_request_number,
 	    user=GH_USERNAME,
 	    repo=GH_REPO)
-	branch = pull_request["head"]["ref"]
-	print branch
+	branch = pull_request.head["ref"]
 	circle_url = get_circle_url(branch, CIRCLE_TOKEN)
 	circle_response = requests.post(circle_url)
 
-	if circle_response.status == 201:
-	    issue_number = issue_comment["issue"]["number"]
-	    issue_comment_id = issue_comment["comment"]["id"]
-	    github.issues.comments.create(
-	        number=issue_number,
-	        message="Running e2e test",
-	        user=GH_USERNAME,
-	        repo=GH_REPO)
+        issue_number = issue_comment["issue"]["number"]
+        issue_comment_id = issue_comment["comment"]["id"]
+        github.issues.comments.create(
+	    number=issue_number,
+	    message="Running e2e test",
+	    user=GH_USERNAME,
+	    repo=GH_REPO)
